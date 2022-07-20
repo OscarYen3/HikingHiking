ae93125a222a408bc23caf33b28f51aac660342c
oscaryen <mapd24th3ee@gmail.com>
2022-07-20 10:54:47 +0800

    add ar

---
+++ Hiking/ARView.swift
@@ -0,0 +1,96 @@
+//
+//  ARView.swift
+//  Hiking
+//
+//  Created by Oscar Yen on 2022/7/20.
+//  Copyright © 2022 OscarYen. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ARView: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addBox()
+        addTapGestureToSceneView()
+        configureLighting()
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func addBox(x: Float = 0, y: Float = 0, z: Float = -0.2) {
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3(x, y, z)
+        
+        sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+    
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+       
+        guard let node = hitTestResults.first?.node else {
+            
+            let hitTestResultsWithFeaturePoints = sceneView.hitTest(tapLocation, types: .featurePoint)
+            
+            if let hitTestResultWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
+                let translation = hitTestResultWithFeaturePoints.worldTransform.translation
+                addBox(x: translation.x, y: translation.y, z: translation.z)
+            }
+            
+            return
+        }
+        
+        node.removeFromParentNode()
+    }
+    
+    func addPaperPlane(x: Float = 0, y: Float = 0, z: Float = -0.5) {
+        //no used
+        guard let paperPlaneScene = SCNScene(named: "paperPlane.scn"), let paperPlaneNode = paperPlaneScene.rootNode.childNode(withName: "paperPlane", recursively: true) else { return }
+        paperPlaneNode.position = SCNVector3(x, y, z)
+        sceneView.scene.rootNode.addChildNode(paperPlaneNode)
+
+    }
+    
+    func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+}
+
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
+}
